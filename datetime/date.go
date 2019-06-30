0cd6658e275dc4d5dd73f998461a12570e276e55
Enix Yu <enixyu@cloudesk.top>
2019-06-30 15:24:07 +0800

    add datetime parse functions

--- datetime/date.go
+++ datetime/date.go
@@ -1,8 +1,12 @@
 package datetime
 
 import (
-	"math"
+	"strconv"
 	"time"
+)
+
+const (
+	ns int64 = 1000000000
 )
 
 // FormatISOTime return a ISO format for given date
@@ -13,9 +17,32 @@ func FormatISOTime(date *time.Time) string {
 
 // UnixNanoTime convert unix nano timestamp to Time
 func UnixNanoTime(ts int64) *time.Time {
-	ns := int64(math.Pow(10, 9))
 	sec := ts / ns
 	nano := ts - sec*ns
 	t := time.Unix(sec, nano)
 	return &t
+}
+
+// UnixTime convert unix timestamp to Time
+func UnixTime(ts int64) *time.Time {
+	t := time.Unix(ts, 0)
+	return &t
+}
+
+// UnixNanoTimeString convert unix nano timestamp to Time
+func UnixNanoTimeString(ts string) (*time.Time, error) {
+	t, err := strconv.ParseInt(ts, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return UnixNanoTime(t), nil
+}
+
+// UnixTimeString convert unix timestamp to Time
+func UnixTimeString(ts string) (*time.Time, error) {
+	t, err := strconv.ParseInt(ts, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return UnixTime(t), nil
 }
--- datetime/date_test.go
+++ datetime/date_test.go
@@ -1,6 +1,7 @@
 package datetime
 
 import (
+	"strconv"
 	"testing"
 	"time"
 )
@@ -21,5 +22,51 @@ func TestUnixNanoTime(t *testing.T) {
 	tc := UnixNanoTime(ts)
 	if tn.Sub(*tc) != 0 {
 		t.Errorf("exp: %v, got: %v", tn, tc)
+	}
+}
+
+func TestUnixTime(t *testing.T) {
+	tn := time.Now()
+	ts := tn.Unix()
+	tc := UnixTime(ts)
+	if tn.Sub(*tc) > 1*time.Second {
+		t.Errorf("exp: %v, got: %v", tn, tc)
+	}
+}
+
+func TestUnixNanoTimeString(t *testing.T) {
+	tn := time.Now()
+	ts := tn.UnixNano()
+	tc, err := UnixNanoTimeString(strconv.FormatInt(ts, 10))
+	if err != nil {
+		t.Error(err)
+	}
+	if tn.Sub(*tc) != 0 {
+		t.Errorf("exp: %v, got: %v", tn, tc)
+	}
+}
+
+func TestUnixTimeString(t *testing.T) {
+	tn := time.Now()
+	ts := tn.Unix()
+	tc, err := UnixTimeString(strconv.FormatInt(ts, 10))
+	if err != nil {
+		t.Error(err)
+	}
+	if tn.Sub(*tc) > 1*time.Second {
+		t.Errorf("exp: %v, got: %v", tn, tc)
+	}
+}
+func TestUnixNanoTimeStringFailed(t *testing.T) {
+	_, err := UnixNanoTimeString("abc")
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestUnixTimeStringFailed(t *testing.T) {
+	_, err := UnixTimeString("abc")
+	if err == nil {
+		t.Fail()
 	}
 }
